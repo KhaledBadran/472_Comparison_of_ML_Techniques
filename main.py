f59e8aa5ac75b2914b0774e96c57063e4381bd29
Khaled Badran <badrankhaled97@gmail.com>
2020-10-14 02:17:07 -0400

    Add initial implementation of GNB and performance metrics

--- main.py
+++ main.py
@@ -7,3 +7,9 @@ from sklearn.metrics import confusion_matrix, classification_report
 
 X_train, y_train, X_validate, y_validate, X_test, y_test = parse_dataset(dataset_number=1)
 
+clf = GaussianNB()
+clf.fit(X_train, y_train)
+prediction = clf.predict(X_test)
+print(prediction[:20])
+print(classification_report(y_true=y_test, y_pred=prediction, zero_division=0))
+print(confusion_matrix(y_true=y_test, y_pred=prediction))
