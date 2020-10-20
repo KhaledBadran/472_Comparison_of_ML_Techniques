edd05d5eb4c8abefa3e33d9c5791b806161c2871
makchamp <makramadaime@hotmail.com>
2020-10-19 20:31:52 -0400

    Add code to read classifier from command line Example: python main.py GNB If no arguments are passed it runs all classifiers for dataset 1 then all of them for dataset 2

--- main.py
+++ main.py
@@ -1,15 +1,28 @@
 from classifiers import Classifiers
-
+import sys
 
 if __name__ == "__main__":
 
-    for dataset in [1, 2]:
+    dataset_index = [1, 2]
+    for dataset in dataset_index:
         classifiers = Classifiers(dataset)
-
-        # call the methods in the Classifiers instance
-        # classifiers.GNB()
-        # classifiers.Base_DT()
-        # classifiers.Best_DT()
-        # classifiers.PER()
-        # classifiers.Base_MLP()
-        classifiers.Best_MLP()
+        clfs = {
+            'GNB': classifiers.GNB,
+            'Base_DT': classifiers.Base_DT,
+            'Best_DT': classifiers.Best_DT,
+            'PER': classifiers.PER,
+            'Base_MLP': classifiers.Base_MLP,
+            'Best_MLP': classifiers.Best_MLP,
+        }
+        try:
+            if len(sys.argv) == 1:  # No extra argument passed, Run all
+                for key, value in clfs.items():
+                    print(f"Running: {key} for dataset {dataset}")
+                    clfs[key]()
+            else:
+                print(f"Running: {sys.argv[1]} for dataset {dataset}")
+                clfs[sys.argv[1]]()
+        except:
+            print(f"Supported paramater list:\n{clfs.keys()}")
+            print("Example: python main.py GNB")
+            sys.exit()
